8ac5b8c4bf5d92a9ac760a5d56716a252ac33ef1
dannyLiu <h152074716@gmail.com>
2020-03-03 00:48:54 +0800

    加入分頁功能

--- src/app/article/data.service.ts
+++ src/app/article/data.service.ts
@@ -6,17 +6,35 @@ import { HttpClient } from '@angular/common/http';
 })
 export class DataService {
 
-data;
+  data;
+  showData;
+  paginArray = [1];
+  nowPage = 1;
+  onePageHaveCount = 10;
   constructor(private httpClient: HttpClient) {
     httpClient.get('https://raw.githubusercontent.com/dannypc1628/w3hexschool-Extension-API/master/data.json')
-    .subscribe(result => {
-      this.data = result;
-      console.log(result);
-    });
+      .subscribe(result => {
+        this.data = result;
+        console.log(result);
+        this.setPaginArry(this.data.length / this.onePageHaveCount + 1);
+        this.goPage(1);
+      });
 
   }
 
-  run() {
-    console.log('data service');
+  setPaginArry(i) {
+    this.paginArray = [];
+    for (let j = 1; j < i; j++) {
+      this.paginArray.push(j);
+    }
+  }
+  goPage(i) {
+    let length = this.onePageHaveCount * i;
+    if (length > this.data.length) {
+      length = this.data.length;
+    }
+    this.showData = this.data.slice(0 + this.onePageHaveCount * (i - 1), length);
+    this.nowPage = i;
+    console.log(this.showData);
   }
 }
